0cb0202a6f2d58ffbfdeff5f86aec960b0cae0f7
Nimra Shahid <shahidnimra08@gmail.comclass>
2024-03-31 07:18:47 +0500

    init

--- LinkedList/add_node/LinkedList.java
+++ LinkedList/add_node/LinkedList.java
@@ -221,7 +221,7 @@ public class LinkedList{
             prev = curr;
             curr = next;
         }
-        Node right  = prev;
+        Node right  = prev;   // midnode
         Node left  = head;
 
         // step 3
@@ -234,19 +234,77 @@ public class LinkedList{
         }
         return true;
     }
+
+    // detect a loop/cycle in a LL
+    //  using floyd's cycle algorithm
+
+    public static boolean detectcycle(){
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //  remove cycle from a linked list
+    // detect cycle 
+    public static void rmeovecycle(){
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+            if(fast == slow){
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false){
+            return;
+        }
+
+        // find meeting point
+        slow = head ;
+        Node prev = head;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // remove
+        prev.next = null;
+    }
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-        ll.print();       
-        ll.addFirst(1);
-        ll.print();
-        ll.addFirst(2);
-        ll.print();
-        ll.addlast(3);
-        ll.print();
-        ll.addlast(4);
-        ll.addmid(2, 9);
-        ll.print();
-        System.out.println(ll.checkpalindrome());
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+        System.out.println(detectcycle());
+        rmeovecycle();
+        System.out.println(detectcycle());
+
+        // LinkedList ll = new LinkedList();
+        // ll.print();       
+        // ll.addFirst(1);
+        // ll.print();
+        // ll.addFirst(2);
+        // ll.print();
+        // ll.addlast(3);
+        // ll.print();
+        // ll.addlast(4);
+        // ll.addmid(2, 9);
+        // ll.print();
+        // System.out.println(ll.checkpalindrome());
+        // System.out.println(ll.detectcycle());
+        // System.out.println(ll.rmeovecycle());
         // System.out.println(ll.size);
 
         // ll.removefirst();
