be65680c2b0db210a5387945d591520af11cf7fe
hanzlikhan <khangormani79@gmail.com>
2024-04-04 05:20:33 +0500

    init

---
+++ Stack/Imp_Problems/Max_Area_Histogram/MaxArea.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+public class MaxArea{
+    public static void  maxArea(int[] height) {
+        int maxar = 0;
+        int nsr[] =  new int[height.length];
+        int nsl[] =  new int[height.length];
+        //  Finding the number of smaller elements on left side
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0;i<height.length;i++){
+            while (!s.isEmpty() && height[s.peek()] >= height[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            }else{
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+        // Finding the number of smaller elements on Right side
+        s = new Stack<>();
+        for(int i = height.length;i>=0;i--){
+            while (!s.isEmpty() && height[s.peek()] >= height[i]) {
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i] = height.length;
+
+            }else{
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+        // find cureent Area
+        for(int i = 0;i<height.length;i++){
+            int hei = height[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = hei * width;
+            maxar = Math.max(maxar, currArea);
+        }
+        System.out.println(maxar);
+    }
+    public static void mint(String[] args) {
+        int height[] = {2,1,5,6,2,3};
+        maxArea(height);
+    }
+}
