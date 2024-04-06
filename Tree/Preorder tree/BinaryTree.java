89d33206418b973b660d8867fc1b6cc163d59eb9
hanzlikhan <khangormani79@gmail.com>
2024-04-06 15:41:07 +0500

    init

--- Tree/Preorder tree/BinaryTree.java
+++ Tree/Preorder tree/BinaryTree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class BinaryTree {
     public static class Node {
         int data;
@@ -24,12 +25,76 @@ public class BinaryTree {
             newNode.right = buildBinary(nodes);
             return newNode;
         }
+
+        // preorder
+        public static void preorder(Node root){
+            if(root==null){
+               return;
+            }   
+            System.out.print(root.data +" ");
+            preorder(root.left);
+            preorder(root.right);
+               
+           }
+
+        //    inorder
+           public static void inorder(Node root){
+            if(root==null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data +" ");
+            inorder(root.right);
+           }
+
+        //    postorder
+        public static void postorder(Node root){
+            if(root==null){
+                return;
+            }
+            inorder(root.left);
+            inorder(root.right);
+            System.out.print(root.data +" ");
+            
+           }
+           // level order traversal
+           public static void Levelorder(Node root){
+            if(root==null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode==null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        return;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" "); 
+                    if(currNode.left!=null){
+                        q.add(currNode.left);
+                    }if (currNode.right !=null) {
+                        q.add(currNode.right);
+                        
+                    }
+                }
+            }
+            
+           }
     }
+    
 
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinTree tree = new BinTree();
         Node root = tree.buildBinary(nodes);
         System.out.println("Inorder root: " + root.data);
+        tree.Levelorder(root);
     }
+    
 }
