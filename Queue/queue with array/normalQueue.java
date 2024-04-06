63854dcdfe6cfb723df2a0212b1d751cf487fd9b
hanzlikhan <khangormani79@gmail.com>
2024-04-06 08:41:15 +0500

    inti

---
+++ Queue/queue with array/normalQueue.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class normalQueue{
+    public class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+         Queue(int n){
+            arr = new int[n];
+            rear = -1;
+            size = n;
+        }
+    }
+    // for empty
+    public static boolean isEmpty(){
+        return rear == -1;
+    } 
+    public static void add(){
+        if(rear== -1){
+            
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
