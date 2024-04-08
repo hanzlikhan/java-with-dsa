65157b0435cbb2aa786cbd7ec9a6b87b1921cd80
hanzlikhan <khangormani79@gmail.com>
2024-04-08 11:29:12 +0500

    init

--- Tree/SubtreeofAnothertree/subTree.java
+++ Tree/SubtreeofAnothertree/subTree.java
@@ -32,5 +32,18 @@ public class subTree{
         }
         return true;
     }
-    
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(4);
+        subRoot.left = new Node(5);
+    isSubroot(root, subRoot);
+    }
 }
