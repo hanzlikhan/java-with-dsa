4e3fdf831e5c8c4802b0edf5a8598862c660d418
hanzlikhan <khangormani79@gmail.com>
2024-04-15 17:49:02 +0500

    init

--- Tree/SubtreeofAnothertree/subTree.java
+++ Tree/SubtreeofAnothertree/subTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class subTree {
     static class Node {
         int data;
@@ -42,6 +44,39 @@ public class subTree {
         klevel(root.right, level+1, k);
     }
 
+    // Lowest commmon ancestor of a tree
+    public static Node lca(node root,int n1,int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+        getpath(root,n1,path1);
+        getpath(root,n2,path2);
+        int i = 0;
+        for(;i<path1.size()&& i<path2.size();i++){
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+
+    // is subtree of another tree getpath function is a function lowest common ancestor
+    public static boolean getpath(Node root,int n, ArrayList<Node>path){
+        if(root == null){
+            return false;
+        }
+        path.add(root);
+        if(root.data == n){
+            return true;
+        }
+        boolean fleft = getpath(root.left, n, path);
+        boolean fright = getpath(root.right, n, path);
+        if(fleft || fright){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
 
     public static void main(String[] args) {
         Node root = new Node(1);
@@ -52,8 +87,11 @@ public class subTree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int k = 2;
-        klevel(root, 1, k);
+        // int k = 2;
+        // klevel(root, 1, k);
+
+        int n1 = 4,n2 = 5;
+        System.out.println(lca(root, n1, n2));
 
         // Node subRoot = new Node(2);
         // subRoot.left = new Node(4);
