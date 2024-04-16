4b4957dff32bb4e02794f9b2db953c927fb9ec12
hanzlikhan <khangormani79@gmail.com>
2024-04-16 15:37:23 +0500

    init

--- Tree/SubtreeofAnothertree/subTree.java
+++ Tree/SubtreeofAnothertree/subTree.java
@@ -90,8 +90,7 @@ public class subTree {
         // int k = 2;
         // klevel(root, 1, k);
 
-        int n1 = 4,n2 = 5;
-        System.out.println(lca(root, n1, n2).data);
+        
 
         // Node subRoot = new Node(2);
         // subRoot.left = new Node(4);
